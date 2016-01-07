3a9295f2eaf62090384430ee3ca6f4980102eb7f
Glen H. Barratt <ghbarratt@gmail.com>
2016-01-07 10:22:26 -0500

    Moved some of these lesser-used functions around a bit. (Consolidating)

--- general_functions.inc.php
+++ general_functions.inc.php
@@ -47,3 +47,17 @@ function getIP()
 	else return $_SERVER['REMOTE_ADDR'];
 	
 }// getIP
+
+
+function forceHTTPS()
+{
+	if(isset($_SERVER))
+	{
+		if(!isset($_SERVER['HTTPS']) || !$_SERVER['HTTPS'])
+		{
+			header('Location: https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+			exit;
+		}
+	}
+}// forceHTTPS
+
